acdb88197e1a1ba6eb4137fc29a3459b5f157d87
Sugyeong <onmay16@gmail.com>
2022-03-08 20:19:16 -0600

    update: settings

--- backEndProject/backEndProject/settings.py
+++ backEndProject/backEndProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'accounts',
+    'application',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
