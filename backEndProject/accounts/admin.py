db43c238eaccfea30955addbe96d680cb43b29dd
Sugyeong <onmay16@gmail.com>
2022-03-08 20:13:25 -0600

    update: admin page

--- backEndProject/accounts/admin.py
+++ backEndProject/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'phone', 'first_name', 'last_name', 'age', 'school_year', 'gpa', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('email', 'first_name', 'last_name')
+
+admin.site.register(User, UserAdmin)
--- backEndProject/application/admin.py
+++ backEndProject/application/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Application, Status
+
 # Register your models here.
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'applicant', 'company', 'position', 'location', 'status', 'date_created', 'last_updated', 'job_post',)
+    list_filter = ('applicant', 'company', 'location',)
+    search_fields = ('company', 'position')
+    ordering = ('date_created', 'last_updated')
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_default', 'created_by')
+    list_filter = ('created_by', 'is_default')
+
+
+
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Status, StatusAdmin)
